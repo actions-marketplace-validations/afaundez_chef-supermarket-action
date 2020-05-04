892f74a51b25c3ae00d24300dc9495e05280b958
Alvaro Faundez <alvaro@faundez.net>
2020-05-03 23:45:29 -0400

    Move options to config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,34 @@
 USER_KEY="/tmp/${USER}.pem";
 echo ${KEY} > ${USER_KEY};
 
-GITHUB_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -f1 -d/);
-GITHUB_PROJECT=$(echo ${GITHUB_REPOSITORY} | cut -f2 -d/);
-
+GITHUB_OWNER=$(echo "${GITHUB_REPOSITORY}" | cut -f1 -d/);
 echo "GITHUB_OWNER: ${GITHUB_OWNER}";
-echo "GITHUB_PROJECT: ${GITHUB_PROJECT}";
-
 if [ -z "${USER}" ]; then
-    USER="${GITHUB_OWNER}";
-    echo "Patching USER with ${USER}.";
+  USER="${GITHUB_OWNER}";
+  echo "Patching USER with ${USER}.";
 fi;
 
+GITHUB_PROJECT=$(echo "${GITHUB_REPOSITORY}" | cut -f2 -d/);
+echo "GITHUB_PROJECT: ${GITHUB_PROJECT}";
 if [ -z "${COOKBOOK}" ]; then
-    COOKBOOK="${GITHUB_PROJECT}";
-    echo "Patching COOKBOOK with ${COOKBOOK}.";
+  COOKBOOK=$(echo "${GITHUB_PROJECT}" | sed -e "s/-cookbook$//");
+  echo "Patching COOKBOOK with ${COOKBOOK}.";
 fi;
 
+echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}";
+ls -larth ${GITHUB_WORKSPACE};
 COOKBOOK_PATH=$(dirname "${GITHUB_WORKSPACE}");
 echo "COOKBOOK_PATH: ${COOKBOOK_PATH}";
 
 echo "Sharing '${COOKBOOK}' with user '${USER}'";
 cd ${GITHUB_WORKSPACE};
-knife supermarket share "${COOKBOOK}" "${CATEGORY}" --user="${USER} "--key="${USER_KEY}.pem" --cookbook-path="${COOKBOOK_PATH}";
+
+CONFIG_PATH="/tmp/config.rb";
+touch ${CONFIG_PATH};
+echo "node_name \"${USER}\"" >> ${CONFIG_PATH};
+echo "client_key \"${USER_KEY}\"" >> ${CONFIG_PATH};
+echo "cookbook_path [ \"${COOKBOOK_PATH}\" ]" >> ${CONFIG_PATH};
+echo "source_url \"https://github.com/${GITHUB_REPOSITORY}\"" >> ${CONFIG_PATH};
+echo "issues_url \"https://github.com/${GITHUB_REPOSITORY}/issues\"" >> ${CONFIG_PATH};
+
+knife supermarket share "${COOKBOOK}" "${CATEGORY}" --config="${CONFIG_PATH}";

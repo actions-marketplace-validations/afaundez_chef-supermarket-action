20118c315cfb21ad4df536f4cfb583e08226e320
Alvaro Faundez <alvaro@faundez.net>
2020-05-03 23:00:23 -0400

    Add debug lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,27 @@
 #!/bin/sh -l
 
-USER_KEY='/tmp/${USER}.pem';
-cat ${KEY} > ${USER_KEY};
+USER_KEY="/tmp/${USER}.pem";
+echo ${KEY} > ${USER_KEY};
 
 GITHUB_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -f1 -d/);
 GITHUB_PROJECT=$(echo ${GITHUB_REPOSITORY} | cut -f2 -d/);
 
-if [ -z '${USER}' ]; then
-    USER='${GITHUB_OWNER}';
-    echo 'Patching user with ${USER}.';
+echo "GITHUB_OWNER: ${GITHUB_OWNER}";
+echo "GITHUB_PROJECT: ${GITHUB_PROJECT}";
+
+if [ -z "${USER}" ]; then
+    USER="${GITHUB_OWNER}";
+    echo "Patching USER with ${USER}.";
 fi;
 
-if [ -z '${COOKBOOK}' ]; then
-    COOKBOOK=$(echo '${GITHUB_PROJECT}' | sed -e 's/-cookbook$//');
-    echo 'Patching cookbook with ${COOKBOOK}.';
+if [ -z "${COOKBOOK}" ]; then
+    COOKBOOK=$(echo "${GITHUB_PROJECT}" | sed -e "s/-cookbook$//");
+    echo "Patching COOKBOOK with ${COOKBOOK}.";
 fi;
 
+COOKBOOK_PATH=$(dirname "${GITHUB_WORKSPACE}");
+echo "COOKBOOK_PATH: ${COOKBOOK_PATH}";
+
+cd "Sharing '${COOKBOOK}' with user '${USER}'";
 cd ${GITHUB_WORKSPACE};
-
-knife supermarket share ${COOKBOOK} ${CATEGORY} --user=${USER} --key=${USER_KEY}.pem;
+knife supermarket share "${COOKBOOK}" "${CATEGORY}" --user="${USER} "--key="${USER_KEY}.pem" --cookbook-path="${COOKBOOK_PATH}";
